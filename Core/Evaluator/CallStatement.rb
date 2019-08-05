5f0725759350b00dbd696c44f3e3893a9770fdbd
thomation <thomation@126.com>
2019-08-05 11:25:14 +0800

    Add nativefunction to calling eval

--- Core/Evaluator/CallStatement.rb
+++ Core/Evaluator/CallStatement.rb
@@ -2,6 +2,15 @@ class CallStatement
     def eval(outer_env)
         f = outer_env.get(name.name)
         raise "undefined name #{name.name}" unless f
+        if f.is_a? NativeFunction
+            eval_native_func()
+        else
+            eval_func(outer_env, f)
+        end
+    end
+    def eval_native_func()
+    end
+    def eval_func(outer_env, f)
         i = 0
         env = f.make_env
         while i < f.params.size do
--- Core/Natives.rb
+++ Core/Natives.rb
@@ -1,8 +1,10 @@
+require_relative 'Evaluator/NativeFunction'
 class Natives
     def env(env)
         return append_natives(env)
     end
     def append_natives(env)
+        env.put_new('print', NativeFunction.new)
         return env
     end
 end
