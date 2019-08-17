48908f296f88c64e2f2ee51c7d568492d1a17936
thomation <thomation@126.com>
2019-08-17 15:34:53 +0800

    Add superclass define

--- Core/ASTree/ClassStatement.rb
+++ Core/ASTree/ClassStatement.rb
@@ -4,12 +4,19 @@ class ClassStatement < ASTBranch
     def name
         child(0)
     end
+    def super_name
+        return child(2) if has_super
+    end
+    def has_super
+        return childrenAmount > 2
+    end
     def body
         child(1)
     end
     def test(level, tag)
         debug_log(level, tag, "ClassStatement")
         name.test(level + 1, "name")
+        super_name.test(level + 1, "super") if has_super
         body.test(level + 1, "body")
     end
 end
--- Core/Parser/ClassParser.rb
+++ Core/Parser/ClassParser.rb
@@ -7,6 +7,12 @@ require_relative '../ASTree/ClassStatement'
 class ClassParser < Parser
     def parse(lexer)      
         lexer.first!
-        return ClassStatement.new([Name.new(lexer.first!), ClassBodyParser.new.parse(lexer)])
+        class_name = Name.new(lexer.first!)
+        if (token = lexer.peek(0)).is_a? KeyToken and token.text == "extends"
+            lexer.first!
+            super_name = Name.new(lexer.first!)
+            return ClassStatement.new([class_name, ClassBodyParser.new.parse(lexer), super_name])
+        end
+        return ClassStatement.new([class_name, ClassBodyParser.new.parse(lexer)])
     end
 end
