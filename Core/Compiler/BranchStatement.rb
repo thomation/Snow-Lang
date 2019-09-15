b9c8b9bb834052576b80820a6dea7ddb57b0927f
thomation <thomation@126.com>
2019-09-15 11:26:22 +0800

    Fix problem that else block will run every time.
    The jump target after every block must be end of the whole if statement.

--- Core/Compiler/BranchStatement.rb
+++ Core/Compiler/BranchStatement.rb
@@ -1,6 +1,7 @@
 require_relative '../Instruction/JumpInstruction'
 class BranchStatement
     def compile(code)
+        @block_finish_jumps = []
         index = 0
         while index * 2 < childrenAmount do
             if is_else? index
@@ -11,6 +12,7 @@ class BranchStatement
 
             index += 1
         end
+        @block_finish_jumps.each{|goto| goto.update_offset_with_current_pos}
     end
     def compile_without_conditon(code, index)
         compile_block(code, index)
@@ -19,7 +21,9 @@ class BranchStatement
         old_reg = code.next_reg
         branch_statement(index).compile(code)
         code.next_reg = old_reg
-        GotoInstruction.new.encode(code)
+        goto = GotoInstruction.new
+        goto.encode(code)
+        @block_finish_jumps << goto
     end
     def compile_with_condition(code, index)
         condition(index).compile(code)
