cae2d6a2957a21643added6108634e90f6652a0a
thomation <thomation@126.com>
2019-09-28 09:44:50 +0800

    Add Opcode class to contain all code to simple management

---
+++ Core/Instruction/Opcode.rb
@@ -0,0 +1,21 @@
+module Opcode
+    ICONST = 1
+    BCONST = 2
+    SCONST = 3
+    
+    LOAD = 11
+    STORE = 12
+    GOTO = 21
+    IFZERO = 22
+
+    NEG = 32
+    ADD = 33
+    SUB = 34
+    MUL = 35
+    DIV = 36
+    REM = 37
+    
+    EQUAL = 41
+    MORE = 42
+    LESS = 43
+end
